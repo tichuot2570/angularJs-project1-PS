e4e5092c6cb8ab436933fcdda328497a0db9837e
namp <tichuot2570@gmail.com>
2017-11-28 13:32:52 +0200

    retrieving data  using HTTP

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StartComponent } from './shared/star.component';
 
 @NgModule({
-  //our directives, components, and pipes are declared
+  // our directives, components, and pipes are declared
   // in declaration array
   declarations: [
     AppComponent,
@@ -19,7 +21,8 @@ import { StartComponent } from './shared/star.component';
   // defined in external Ang module we add to the imports array
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { IProduct } from './product';
 import { OnDestroy, OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 import { ProductService } from './product.service';
+import { error } from 'util';
 
 
 @Component({
@@ -15,29 +16,30 @@ export class ProductListComponent implements OnInit {
     pageTitle: string = 'Product List';
     imageWidth: number = 100;
     imageMargin: number = 2;
-    //event binding
+    // event binding
     showImage: boolean = false;
-    //use get-set for filtering
+    errorMessage: string;
+    // use get-set for filtering
     _listFilter: string;
-    //when the data binding needs the value, it will call the getter and get the value
+    // when the data binding needs the value, it will call the getter and get the value
     get listFilter(){
         return this._listFilter;
     }
-    //everytime the user modifies the value, the data binding calls the setter, passing in the changed value
-    //if we wanna perform some logic everytime the value is changed, we can add it here in the setter
-    set listFilter(value:string){
+    // everytime the user modifies the value, the data binding calls the setter, passing in the changed value
+    // if we wanna perform some logic everytime the value is changed, we can add it here in the setter
+    set listFilter(value: string){
         this._listFilter = value;
         this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter): this.products;
     }
 
-    //7.filtering a list
+    // 7.filtering a list
     filteredProducts: IProduct[];
     products: IProduct[] = [];
-    //the best place to set default values for more complex properties is in the class constructor.
-    //Class constructor is a function executed when the component is first initialized.
+    // the best place to set default values for more complex properties is in the class constructor.
+    // Class constructor is a function executed when the component is first initialized.
     constructor(private _productService: ProductService){
-        //this.filteredProducts = this.products;
-        //this.listFilter = 'cart';
+        // this.filteredProducts = this.products;
+        // this.listFilter = 'cart';
 
     }
 
@@ -47,25 +49,30 @@ export class ProductListComponent implements OnInit {
 
     performFilter(filterBy: string): IProduct[]{
         filterBy = filterBy.toLocaleLowerCase();
-        //Let's look closer at the filter method call. We are using the array filter method filter method to create
-        //a new array with elements that pass the test defined in the provided function. 
-        // use the ES 2015 arrow syntax to define that filter function. For each product in the list, 
-        //the product name is converted to lowercase and index OF is used to determine if 
-        //the filter text is found in the product name- If so, the element is added to the filtered list.
-        //check filter and IndexOf: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filternpm st
-        return this.products.filter((product:IProduct) => 
+        // Let's look closer at the filter method call. We are using the array filter method filter method to create
+        // a new array with elements that pass the test defined in the provided function.
+        // use the ES 2015 arrow syntax to define that filter function. For each product in the list,
+        // the product name is converted to lowercase and index OF is used to determine if
+        // the filter text is found in the product name- If so, the element is added to the filtered list.
+        // check filter and IndexOf: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filternpm st
+        return this.products.filter((product: IProduct) =>
         product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
     }
 
-    //showImage button is actived to hide images
+    // showImage button is actived to hide images
     toggleImage(): void {
         this.showImage = !this.showImage;
     }
-    //this provides a place to perform any component initialization and a greeat place to retrieve data the template
-    //cuz the connstructor will be first initialized, we have to move 
+    // this provides a place to perform any component initialization and a greeat place to retrieve data the template
+    // cuz the connstructor will be first initialized, we have to move
     // this.filteredProducts = this.products; to ngInit()
     ngOnInit(): void {
-        this.products  = this._productService.getProducts();
-        this.filteredProducts = this.products;
+        this._productService.getProducts()
+        .subscribe (products => {
+          this.products = products;
+          this.filteredProducts = this.products;
+        },
+          error => this.errorMessage = <any>error);
+        // this.filteredProducts = this.products;
     }
 }
